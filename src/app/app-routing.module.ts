80e8c94e74a3892ff90dd94a8c5e9e2fe04ab32c
Puesto 1 Gicoop <puesto1gicoop@Mac-mini-de-Puesto.local>
2023-07-11 08:34:08 +0200

    create all structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/workout', pathMatch: 'full' },
+  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'profile', loadChildren: () => import('./modules/profile/profile.module').then(m => m.ProfileModule) },
+  { path: 'workout', loadChildren: () => import('./modules/workout/workout.module').then(m => m.WorkoutModule) },
+  { path: 'results', loadChildren: () => import('./modules/results/results.module').then(m => m.ResultsModule) },
+  { path: '**', redirectTo: '/workout' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
